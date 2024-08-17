324b9770003c944db324b243f98a32e1aafe70cd
Dominic Dang <dominicdang@protonmail.com>
2024-08-17 05:11:45 +0000

    simple rebrand

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -61,7 +61,7 @@ const LoginModal = () => {
 	const footerContent = (
 		<div className="text-neutral-400 text-center mt-4">
 			<p>
-				First time using Twitter?{" "}
+				First time using Y Not?{" "}
 				<span
 					onClick={onToggle}
 					className="text-white cursor-pointer hover:underline"
